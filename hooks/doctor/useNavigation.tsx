fdb885835467e489d72cb59f79d5e602896a07f5
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-12-24 23:20:54 +0300

    efficency edit

--- hooks/doctor/useNavigation.tsx
+++ hooks/doctor/useNavigation.tsx
@@ -16,37 +16,15 @@ const useNavigation = (): any => {
     const [ isHelpActive, setIsHelpActive ] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsDashboardActive(false);
-    setIsEducationCenterActive(false);
-    setIsMedicalhistoryActive(false);
-    setIsActivevisitsActive(false);
-    setIsChatActive(false);
-    setIsSettingActive(false);
-    setIsPaymentActive(false);
-    setIsAccountsActive(false);
-    setIsHelpActive(false);
-
-
-    if (pathname === "/doctor/dashboard") {
-        setIsDashboardActive(true);
-    } else if (pathname.includes("/doctor/medicalhistory")) {
-        setIsMedicalhistoryActive(true);
-    } else if (pathname.includes("/doctor/activevisits")) {
-        setIsActivevisitsActive(true);
-    } else if (pathname.includes("/doctor/education")) {
-        setIsEducationCenterActive(true);
-    } else if (pathname.includes("/doctor/chat")) {
-        setIsChatActive(true);
-    } else if (pathname.includes("/doctor/settings")) {
-        setIsSettingActive(true);
-    } else if (pathname.includes("/doctor/payment")) {
-        setIsPaymentActive(true);
-    } else if (pathname.includes("/doctor/accounts")) {
-        setIsAccountsActive(true);
-    } else if (pathname.includes("/doctor/help")) {
-        setIsHelpActive(true);
-    }
-
+    setIsDashboardActive(pathname.includes("/doctor/dashboard"));
+    setIsEducationCenterActive(pathname.includes("/doctor/education"));
+    setIsMedicalhistoryActive(pathname.includes("/doctor/medicalhistory"));
+    setIsActivevisitsActive(pathname.includes("/doctor/activevisits"));
+    setIsChatActive(pathname.includes("/doctor/chat"));
+    setIsSettingActive(pathname.includes("/doctor/settings"));
+    setIsPaymentActive(pathname.includes("/doctor/payment"));
+    setIsAccountsActive(pathname.includes("/doctor/accounts"));
+    setIsHelpActive(pathname.includes("/doctor/help"));
 
   }, [pathname]);
 
