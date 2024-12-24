b0b37305f87b90e884bc4cdd0068a351a6749959
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-12-24 23:39:05 +0300

    add middleware for default routing

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const middleware = async (req: NextRequest): Promise<NextResponse> => {
+  const { pathname } = req.nextUrl;
+  const url = req.nextUrl.clone();
+
+
+  if (pathname === "/doctor") {
+    url.pathname = "/doctor/dashboard";
+    return NextResponse.redirect(url);
+}
+
+
+    return NextResponse.next();
+};
+
+export const config = {
+    matcher: ["/doctor/:path*"]
+};
+
+
