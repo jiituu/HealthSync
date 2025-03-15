18af096ef97d1dbf2ea15d82bf93c65f72d0fcf6
Epyuel <epdemrew@gmail.com>
2025-03-15 21:58:06 +0300

    SCRUM-51 - Inprogress

--- redux/api/endpoints.tsx
+++ redux/api/endpoints.tsx
@@ -29,7 +29,7 @@ export const endpointsApiFunc = createApi({
       query: (doctor) => ({
         url: '/doctors',
         method: 'POST',
-        body: JSON.stringify(doctor)
+        body: doctor
       }),
     }),
 
